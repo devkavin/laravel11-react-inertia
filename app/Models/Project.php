ee58ea095a93d950e3257f9a0e2eeb9b32c9ab8e
devkavin <kavindra.senanayake@gmail.com>
2024-04-02 22:23:22 +0530

    projects created, sort fields added

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -13,4 +13,18 @@ class Project extends Model
     {
         return $this->hasMany(Task::class);
     }
+
+    /**
+     * Get the user that owns the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by',);
+    }
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by',);
+    }
 }
