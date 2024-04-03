b68fa3fcff79440938bf4640a487eb517c7c9a01
devkavin <kavindra.senanayake@gmail.com>
2024-04-03 20:21:08 +0530

    Add comment in Index.jsx file

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -49,7 +49,7 @@ export default function Index({ auth, projects, queryParams }) {
             }
         >
             <Head title="Projects" />
-
+            {/* comment */}
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
